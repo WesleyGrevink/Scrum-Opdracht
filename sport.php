a8b6557a8dfbf0b9fe1eac41c5c2588dd298a9f6
WesleyGrevink <43266153+WesleyGrevink@users.noreply.github.com>
2019-04-17 22:00:56 +0200

    Pagina Sport voorzien van een deel informatie

--- sport.php
+++ sport.php
@@ -1,1 +1,29 @@
 <?php include("source/header.php") ?>
+<main>
+  <div class="container">
+    <ul class="collapsible" style="background-color: white;">
+      <li>
+        <div class="collapsible-header">
+          <i class="material-icons">filter_drama</i>
+          Over FC Emmen
+          <span class="new badge">4</span></div>
+        <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+      </li>
+      <li>
+        <div class="collapsible-header">
+          <i class="material-icons">place</i>
+            Locatie Stadion Emmen
+          <span class="badge">1</span></div>
+          <div class="collapsible-body iframecustom"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d717.5793202148616!2d6.944639400553937!3d52.774775492721865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b7e8c0ee8dd521%3A0xc8247ad008a33721!2sJENS+Vesting!5e0!3m2!1sen!2snl!4v1548837585273" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+      </li>
+    </ul>
+  </div>
+
+
+
+</main>
+<script>
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+</script>
